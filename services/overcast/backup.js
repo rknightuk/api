47ccac0551b921d70b7352d65821b1765d5d6b94
Robb Knight <desk@rknight.me>
2023-01-20 23:25:18 +0000

    Chaneg backup scrupt

--- services/overcast/backup.js
+++ services/overcast/backup.js
@@ -3,8 +3,8 @@ import store from '../../utils/store.js'
 
 function run() {
   try {
-    fs.copyFileSync('./services/overcast/overcast.opml', './services/overcast/overcast-backup.opml', fs.constants.COPYFILE_FICLONE)
-    fs.copyFileSync('./api/podcasts.json', './api/podcasts-backup.json', fs.constants.COPYFILE_FICLONE)
+    fs.copyFileSync('./services/overcast/overcast.opml', `./services/overcast/overcast-${new Date().toDateString().replaceAll(' ', '-')}backup.opml`, fs.constants.COPYFILE_FICLONE)
+    fs.copyFileSync('./api/podcasts.json', `./api/podcasts-backup-${new Date().toDateString().replaceAll(' ', '-')}.json`, fs.constants.COPYFILE_FICLONE)
   } catch (error) {
     console.log(error);
   }
