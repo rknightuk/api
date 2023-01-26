3093718ad68588760e4a0f0df3018e25deb0ce9a
Robb Knight <desk@rknight.me>
2023-01-26 14:20:50 +0000

    fix

--- now/generate.js
+++ now/generate.js
@@ -75,7 +75,7 @@ ${nowPageLink}
     }).join('\n'),
     Podcasts: data.podcasts.slice(0, 5).map((p, i) => `- [${p.title}](${p.url}) {${getIcon('podcast', i)}}`).join('\n'),
     Tracks: data.music.tracks.map((t, i) => `- [${t.name} by ${t.artist}](${t.link}) {${getIcon('music', i)}}`).join('\n'),
-    Artists: data.music.artists.map((a, i) => `- [${a.name}](${t.link}) {${getIcon('music', i)}}`).join('\n'),
+    Artists: data.music.artists.map((a, i) => `- [${a.name}](${a.link}) {${getIcon('music', i)}}`).join('\n'),
 //     Artists: `
 // <div class="now_albums">
 //     ${data.music.artists.map(b => {
