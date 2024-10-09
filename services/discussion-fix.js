25b9cd3f515968cf4cbf91dfa736c89c2d4ee51f
Robb Knight <desk@rknight.me>
2024-10-09 16:53:59 +0100

    Remove non-blog posts

--- services/discussion-fix.js
+++ services/discussion-fix.js
@@ -7,17 +7,19 @@ async function run() {
       let fixed = {}
 
       Object.keys(discussion).forEach(key => {  
-        fixed[key] = {}
-        Object.keys(discussion[key]).forEach(toot => {
-            fixed[key][toot] = {
-                content: discussion[key][toot].content,
-                created_at: discussion[key][toot].created_at,
-                url: discussion[key][toot].url,
-                replies_count: discussion[key][toot].replies_count,
-                reblogs_count: discussion[key][toot].reblogs_count,
-                favourites_count: discussion[key][toot].favourites_count,
-            }
-        })
+        if (key.includes('/blog/')) {
+            fixed[key] = {}
+            Object.keys(discussion[key]).forEach(toot => {
+                fixed[key][toot] = {
+                    content: discussion[key][toot].content,
+                    created_at: discussion[key][toot].created_at,
+                    url: discussion[key][toot].url,
+                    replies_count: discussion[key][toot].replies_count,
+                    reblogs_count: discussion[key][toot].reblogs_count,
+                    favourites_count: discussion[key][toot].favourites_count,
+                }
+            })
+        }
       })
   
       fs.writeFileSync('./api/discussion.json', JSON.stringify(fixed))
